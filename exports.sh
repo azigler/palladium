8a86d19345791ab79e9c83ea5dcba281a40cf515
Andrew Zigler <andrewzigler@gmail.com>
2022-12-23 13:49:28 -0800

    Update exports.sh

--- exports.sh
+++ exports.sh
@@ -7,29 +7,26 @@ export PALLADIUM_DOCKER_NETWORK="palladium_default"
 export PALLADIUM_ADMIN_EMAIL="admin@example.com"
 
 # databases
-export PALLADIUM_MARIADB_ROOT_PASSWORD="mariadb_root_password"
 export PALLADIUM_POSTGRES_ROOT_PASSWORD="postgres_root_password"
+export PALLADIUM_REDIS_PASSWORD="redis_password"
 
-# healthchecks
-export PALLADIUM_HEALTHCHECKS_PASSWORD="healthchecks_password"
+# listmonk
+export PALLADIUM_LISTMONK_POSTGRES_USER="listmonk_postgres_user"
+export PALLADIUM_LISTMONK_POSTGRES_PASSWORD="listmonk_postgres_password"
+export PALLADIUM_LISTMONK_ADMIN_USER="listmonk_admin_user"
+export PALLADIUM_LISTMONK_ADMIN_PASSWORD="listmonk_admin_password"
 
 # mattermost
-export PALLADIUM_MATTERMOST_POSTGRES_USER="mattermost_user"
-export PALLADIUM_MATTERMOST_POSTGRES_PASSWORD="mattermost_password"
-
-# nextcloud
-export PALLADIUM_MARIADB_NEXTCLOUD_USER="nextcloud_user"
-export PALLADIUM_MARIADB_NEXTCLOUD_PASSWORD="nextcloud_password"
-
-# smtp
-export PALLADIUM_SMTP_PORT=25
-export PALLADIUM_POSTMASTER_USER="postmaster@example.com"
-export PALLADIUM_POSTMASTER_PASSWORD="postmaster_password"
+export PALLADIUM_MATTERMOST_POSTGRES_USER="mattermost_postgres_user"
+export PALLADIUM_MATTERMOST_POSTGRES_PASSWORD="mattermost_postgres_password"
+export PALLADIUM_MATTERMOST_ADMIN_USER="mattermost_admin_user"
+export PALLADIUM_MATTERMOST_ADMIN_PASSWORD="mattermost_admin_password"
 
 # swag
-# SEE: https://developers.cloudflare.com/api/tokens/create/ (use "Edit zone DNS" template)
 export PALLADIUM_CLOUDFLARE_TOKEN="your-token-here"
 
 # wikijs
-export PALLADIUM_MARIADB_WIKIJS_USER="wikijs_user"
-export PALLADIUM_MARIADB_WIKIJS_PASSWORD="wikijs_password"
+export PALLADIUM_WIKIJS_POSTGRES_USER="wikijs_postgres_user"
+export PALLADIUM_WIKIJS_POSTGRES_PASSWORD="wikijs_postgres_password"
+export PALLADIUM_WIKIJS_ADMIN_USER="wikijs_admin_user"
+export PALLADIUM_WIKIJS_ADMIN_PASSWORD="wikijs_admin_password"
